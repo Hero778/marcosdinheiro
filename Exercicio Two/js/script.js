7629a1392511fdef9811f3c6142e3d1242807932
Marcos <39920864+Hero778@users.noreply.github.com>
2018-07-25 13:59:34 -0300

    Todos os arquivos
    asdasd

---
+++ Exercicio Two/js/script.js
@@ -0,0 +1,15 @@
+var nome=prompt("Digite Seu Nome");
+alert(nome  + "VOCE TEM UM VIRUS Q HACKEIA SUA VIDA EM 5 SEGUNDOS DEPOSITE 500 REAIS NA CONTA 453464 E N RECEBA O DEMONIO");
+//var primeiroNome="Marcos";
+//alert( "este e o primeiro valor da variavel   "+Nome );
+//alert(teste1)
+var soma1= +prompt("Escreva numeros");
+var soma2= +prompt("Escreva o segundo");
+var soma = soma1+soma2;
+alert(soma);
+
+var multiplicacao = soma1*soma2;
+alert(soma);
+
+
+
